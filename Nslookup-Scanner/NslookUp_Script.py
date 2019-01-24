e549385c79594f7c70db5b0174bd0b74d57a2663
srimani-programmer <srimani.scholar@gmail.com>
2019-01-24 06:55:51 +0530

    Final commit for the scanner tool.

--- Nslookup-Scanner/NslookUp_Script.py
+++ Nslookup-Scanner/NslookUp_Script.py
@@ -1,34 +1,61 @@
 # Nslookup Script in Python.
-# done by Sri ManikantaPalakollu.
+# Date 24/01/2019. 
+# Done by Sri ManikantaPalakollu.
 # Python v3.6.7
 
 import socket
 
-while True:
+# Banner function is used to create the banner on the terminal.
+def banner():
+    print('''
+                                           
+   (_) _       (_)                (_)                                         (_)                                        
+   (_)(_)_     (_)   _  _  _  _   (_)               _  _  _        _  _  _    (_)     _  _         _    _  _  _  _       
+   (_)  (_)_   (_) _(_)(_)(_)(_)  (_)            _ (_)(_)(_) _  _ (_)(_)(_) _ (_)   _(_)(_)       (_)  (_)(_)(_)(_)_     
+   (_)    (_)_ (_)(_)_  _  _  _   (_)           (_)         (_)(_)         (_)(_) _(_)  (_)       (_)  (_)        (_)    
+   (_)      (_)(_)  (_)(_)(_)(_)_ (_)           (_)         (_)(_)         (_)(_)(_)_   (_)       (_)  (_)        (_)    
+   (_)         (_)   _  _  _  _(_)(_) _  _  _  _(_) _  _  _ (_)(_) _  _  _ (_)(_)  (_)_ (_)_  _  _(_)_ (_) _  _  _(_)    
+   (_)         (_)  (_)(_)(_)(_)  (_)(_)(_)(_)(_)  (_)(_)(_)      (_)(_)(_)   (_)    (_)  (_)(_)(_) (_)(_)(_)(_)(_)      
+                                                                                                       (_)               
+                                                                                                       (_)               
+    
+    ''')
 
-    print('1. IP ADDRESS FINDER.')
-    print('2. SERVER ADDRESS FINDER.')
-    print('3. Exit')
-    choice = input('Enter your Choice:')
+# Scanner() is used to perform the nslookup and reverse nslookup operation.
 
-    try:
-        if int(choice) == 1:
-            url = input('Enter a valid URL without HTTP/HTTPS:')
-            print('Non-authoritative answer:')
-            print('Name : {}'.format(url))
-            print('Address : {}'.format(socket.gethostbyname(url)))
-            print('\n')
-        elif int(choice) == 2:
-            url = input('Enter a Ip Address:')
-            print('Address : {}'.format(socket.gethostbyaddr(url)[0]))
-            print('\n')
-        elif int(choice) == 3:
-            print('Thanks for Using it.')
-            print('Scripted by Sri Manikanta.')
-            break
-        else:
-            print('Please Enter a valid URL')
-    except socket.error:
-        print(socket.error)
+def scanner():
+    while True:
 
+        # Options for the user.
+        print('1. IP ADDRESS FINDER.')
+        print('2. SERVER ADDRESS FINDER.')
+        print('3. Exit')
+        choice = input('Enter your Choice:')
 
+        try:
+            if int(choice) == 1:    # it returns the ipaddress as the output wich is nslookup scan.
+                url = input('Enter a valid URL without HTTP/HTTPS:')
+                print('\n')
+                print('Non-authoritative answer:')
+                print('Name : {}'.format(url))
+                print('Address : {}'.format(socket.gethostbyname(url)))
+                print('\n')
+            elif int(choice) == 2:  # it returns the server name as the outpuit which is the reverse nslookup scan.
+                url = input('Enter a Ip Address:')
+                print('\n')
+                print('Address : {}'.format(socket.gethostbyaddr(url)[0]))
+                print('\n')
+            elif int(choice) == 3:
+                print('Thanks for Using it.')
+                print('Script written by Sri Manikanta.')
+                break
+            else:
+                print('Please Enter a valid URL')
+        except socket.error:    # If any exception raised in try block it will produce the error message.
+            print(socket.error)
+
+# program start point.
+
+if __name__  ==  '__main__':
+    banner()
+    scanner()
