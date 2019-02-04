6276ea8248f3aa21148ee7827652c45e663c4452
srimani-programmer <srimani.scholar@gmail.com>
2019-02-04 20:53:39 +0530

    Banners and Color codes added.

--- Nslookup-Scanner/NslookUp_Script.py
+++ Nslookup-Scanner/NslookUp_Script.py
@@ -1,25 +1,21 @@
 # Nslookup Script in Python.
 # Date 24/01/2019. 
 # Done by Sri ManikantaPalakollu.
-# Python v3.6.7
+# Python v3.7.2
 
 import socket
+from termcolor import colored,cprint
 
 # Banner function is used to create the banner on the terminal.
 def banner():
-    print('''
-                                           
-   (_) _       (_)                (_)                                         (_)                                        
-   (_)(_)_     (_)   _  _  _  _   (_)               _  _  _        _  _  _    (_)     _  _         _    _  _  _  _       
-   (_)  (_)_   (_) _(_)(_)(_)(_)  (_)            _ (_)(_)(_) _  _ (_)(_)(_) _ (_)   _(_)(_)       (_)  (_)(_)(_)(_)_     
-   (_)    (_)_ (_)(_)_  _  _  _   (_)           (_)         (_)(_)         (_)(_) _(_)  (_)       (_)  (_)        (_)    
-   (_)      (_)(_)  (_)(_)(_)(_)_ (_)           (_)         (_)(_)         (_)(_)(_)_   (_)       (_)  (_)        (_)    
-   (_)         (_)   _  _  _  _(_)(_) _  _  _  _(_) _  _  _ (_)(_) _  _  _ (_)(_)  (_)_ (_)_  _  _(_)_ (_) _  _  _(_)    
-   (_)         (_)  (_)(_)(_)(_)  (_)(_)(_)(_)(_)  (_)(_)(_)      (_)(_)(_)   (_)    (_)  (_)(_)(_) (_)(_)(_)(_)(_)      
-                                                                                                       (_)               
-                                                                                                       (_)               
-    
-    ''')
+    logo = '''  _   _     _                _                
+ | \ | |___| |    ___   ___ | | ___   _ _ __  
+ |  \| / __| |   / _ \ / _ \| |/ / | | | '_ \ 
+ | |\  \__ \ |__| (_) | (_) |   <| |_| | |_) |
+ |_| \_|___/_____\___/ \___/|_|\_\\__,_| .__/ 
+                                       |_|    
+            '''
+    cprint(logo,color='red',attrs=['bold'])
 
 # Scanner() is used to perform the nslookup and reverse nslookup operation.
 
@@ -36,18 +32,18 @@ def scanner():
             if int(choice) == 1:    # it returns the ipaddress as the output wich is nslookup scan.
                 url = input('Enter a valid URL without HTTP/HTTPS:')
                 print('\n')
-                print('Non-authoritative answer:')
-                print('Name : {}'.format(url))
-                print('Address : {}'.format(socket.gethostbyname(url)))
+                cprint('Non-authoritative answer:',color='cyan',attrs=['bold'])
+                cprint('Name : {}'.format(url),color='cyan',attrs=['bold'])
+                cprint('Address : {}'.format(socket.gethostbyname(url)),color='cyan',attrs=['bold'])
                 print('\n')
             elif int(choice) == 2:  # it returns the server name as the outpuit which is the reverse nslookup scan.
                 url = input('Enter a Ip Address:')
                 print('\n')
-                print('Address : {}'.format(socket.gethostbyaddr(url)[0]))
+                cprint('Address : {}'.format(socket.gethostbyaddr(url)[0]),color='cyan',attrs=['bold'])
                 print('\n')
             elif int(choice) == 3:
-                print('Thanks for Using it.')
-                print('Script written by Sri Manikanta.')
+                cprint('Thanks for Using it.',color='white',attrs=['bold'])
+                cprint('Script written by Sri Manikanta.',color='white',attrs=['bold'])
                 break
             else:
                 print('Please Enter a valid URL')
